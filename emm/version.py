3f60f537afc63b2cb8f6332feaf23d438fae7d76
Simon Brugman <sfbbrugman@gmail.com>
2023-12-08 16:49:28 +0100

    REL: version bump to v2.0.0

--- emm/version.py
+++ emm/version.py
@@ -17,7 +17,6 @@
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-# When increasing the version number manually do not forget to update CHANGES.md
-VERSION = "1.4.1"
+VERSION = "2.0.0"
 
 __version__ = VERSION
